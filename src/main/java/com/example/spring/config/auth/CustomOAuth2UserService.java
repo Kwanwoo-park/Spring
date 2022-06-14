db8af081569c0b5090d1686cfe224abc4077baa6
Kwanwoo-park <65990897+Kwanwoo-park@users.noreply.github.com>
2022-06-14 22:28:06 +0900

    Fourth Day

--- src/main/java/com/example/spring/config/auth/CustomOAuth2UserService.java
+++ src/main/java/com/example/spring/config/auth/CustomOAuth2UserService.java
@@ -1,2 +1,59 @@
-package com.example.spring.config.auth;public class CustomOAuth2UserService {
+package com.example.spring.config.auth;
+
+import com.example.spring.config.auth.dto.OAuthAttributes;
+import com.example.spring.config.auth.dto.SessionUser;
+import com.example.spring.domain.user.User;
+import com.example.spring.domain.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Service;
+
+import javax.servlet.http.HttpSession;
+import java.util.Collections;
+
+@RequiredArgsConstructor
+@Service
+public class CustomOAuth2UserService implements OAuth2UserService<OAuth2UserRequest, OAuth2User> {
+    private final UserRepository userRepository;
+    private final HttpSession httpSession;
+
+    @Override
+    public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+        OAuth2UserService<OAuth2UserRequest, OAuth2User> delegate = new DefaultOAuth2UserService();
+        OAuth2User oAuth2User = delegate.loadUser(userRequest);
+
+        String registrationId = userRequest.getClientRegistration().getRegistrationId();/*-현재 로그인 진행 중인 서비스 구분
+                                                                                   -네이버 로그인 연동시 구글과 네이버 구분*/
+        String userNameAttributeName = userRequest.getClientRegistration().getProviderDetails()
+                .getUserInfoEndpoint().getUserNameAttributeName(); /*   -OAuth2 Login 진행 시 키가 되는 필드 값을 이야기
+                                                                        (==Primary key)
+                                                                        -구글의 경우 기본적으로 코드를 지원,
+                                                                        네이버나 카카오는 지원 X, 구글의 기본 코드:sub
+                                                                        -네이버랑 구글 동시 지원시 사용*/
+
+        OAuthAttributes attributes = OAuthAttributes.of(registrationId, userNameAttributeName,
+                oAuth2User.getAttributes()); /* -OAuth2UserServie를 통해 가져온 OAuth2user attribute를 담을 클래스
+                                                -이후 네이버 등 다른 소셜 로그인도 이 클래스를 사용*/
+
+        User user = saveOrUpdate(attributes);
+        httpSession.setAttribute("user", new SessionUser(user)); //세션에 사용자 정보를 저장하기 위한 Dto class
+
+        return new DefaultOAuth2User(Collections.singleton(new SimpleGrantedAuthority(user.getRoleKey())),
+                attributes.getAttributes(), attributes.getNameAttributeKey());
+    }
+
+    private User saveOrUpdate(OAuthAttributes attributes) {
+        User user = userRepository.findByEmail(attributes.getEmail())
+                .map(entity -> entity.update(attributes.getName(),
+                        attributes.getPicture()))
+                .orElse(attributes.toEntity());
+
+        return userRepository.save(user);
+    }
 }
--- src/main/java/com/example/spring/web/IndexController.java
+++ src/main/java/com/example/spring/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.example.spring.web;
 
+import com.example.spring.config.auth.dto.SessionUser;
 import com.example.spring.service.posts.PostsService;
 import com.example.spring.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -8,16 +9,28 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
     public String index(Model model) {/*    -서버 템플릿 엔진에서 사용할 수 있는 객체를 저장할 수 있습니다.
                                             -여기서는 postsService.findAllDesc()로 가져온 결과를 posts로
                                             index.mustache에 전달합니다.*/
         model.addAttribute("posts", postsService.findAllDesc());
+        SessionUser user = (SessionUser) httpSession.getAttribute("user"); /*   -앞서 작성된 CustomOAuth2UserService
+        에서 로그인 성공시 세션에 SessionUser를 저장하도록 구성 즉, 로그인 성공 시 httpSession.getAttribute("user")에서 값을
+        가지고 올 수 있습니다.*/
+
+        if (user != null) { /*  -세션에 저장된 갑싱 있을 때만 model에 userName으로 등록합니다.
+                                -세션에 저장된 값이 없으면 model엔 아무런 값이 없어서 로그인 버튼이 보입니다.*/
+            model.addAttribute("userName", user.getName());
+        }
+
         return "index";
     }
 
