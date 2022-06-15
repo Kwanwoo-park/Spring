d943ec4aef7ebfcbf9ba0583e306d369c447ae2e
Kwanwoo-park <65990897+Kwanwoo-park@users.noreply.github.com>
2022-06-15 21:17:45 +0900

    Fifth Day

--- src/main/java/com/example/spring/web/IndexController.java
+++ src/main/java/com/example/spring/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.example.spring.web;
 
+import com.example.spring.config.auth.LoginUser;
 import com.example.spring.config.auth.dto.SessionUser;
 import com.example.spring.service.posts.PostsService;
 import com.example.spring.web.dto.PostsResponseDto;
@@ -18,11 +19,14 @@ public class IndexController {
     private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {/*    -서버 템플릿 엔진에서 사용할 수 있는 객체를 저장할 수 있습니다.
+    public String index(Model model, @LoginUser SessionUser user) {/*    -서버 템플릿 엔진에서 사용할 수 있는
+                                                                            객체를 저장할 수 있습니다.
                                             -여기서는 postsService.findAllDesc()로 가져온 결과를 posts로
-                                            index.mustache에 전달합니다.*/
+                                            index.mustache에 전달합니다.
+                                            -기존에 (User) httpSession.getAttribute("user")로 가져오던 세션 정보 값이 개선
+                                            -이제는 어느 컨트롤러든지 @LoginUser만 사용하면 세션 정보를 가져올 수 있습니다.*/
         model.addAttribute("posts", postsService.findAllDesc());
-        SessionUser user = (SessionUser) httpSession.getAttribute("user"); /*   -앞서 작성된 CustomOAuth2UserService
+        /*SessionUser user = (SessionUser) httpSession.getAttribute("user");   -앞서 작성된 CustomOAuth2UserService
         에서 로그인 성공시 세션에 SessionUser를 저장하도록 구성 즉, 로그인 성공 시 httpSession.getAttribute("user")에서 값을
         가지고 올 수 있습니다.*/
 
--- src/test/java/com/example/spring/web/HelloControllerTest.java
+++ src/test/java/com/example/spring/web/HelloControllerTest.java
@@ -1,9 +1,13 @@
 package com.example.spring.web;
 
+import com.example.spring.config.auth.SecurityConfig;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -15,16 +19,21 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @RunWith(SpringRunner.class)/*  -테스트를 진행할 때 JUnit에 내장된 실행자 외에 다른 실행자를 실행시킵니다.
                                 -여기서는 SpringRunner라는 스프링 실행자를 사용합니다.
                                 -즉, 스프링 부트 테스트와 JUnit 사이에 연결자 역할을 합니다.*/
-@WebMvcTest(controllers = HelloController.class)/*  -여러 스프링 테스트 어노테이션 중, Web(Spring MVC)에 집중할 수 있는 어노테이션입니다.
+@WebMvcTest(controllers = HelloController.class, /*  -여러 스프링 테스트 어노테이션 중, Web(Spring MVC)에 집중할 수 있는 어노테이션입니다.
                                                     -선언할 경우 @Controller, @ControllerAdvice를 사용할 수 있습니다.
                                                     -단, @Service, @Component, @Repository등은 사용할 수 없습니다.
                                                     -여기서는 컨트롤러만 사용하기 때문에 선언합니다.*/
+        excludeFilters = {
+                @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = SecurityConfig.class)
+        }
+)
 public class HelloControllerTest {
     @Autowired // -스프링이 관리하는 빈(Bean)을 주입 받습니다.
     private MockMvc mvc; /* -웹 API를 테스트할 때 사용합니다.
                             -스프링 MVC 테스트의 시작접입니다.
                             -이 클래스를 통해 HTTP GET, POST 등에 대한 API 테스트를 할 수 있습니다.*/
 
+    @WithMockUser(roles = "USER")
     @Test
     public void hello가_리턴된다() throws Exception {
         String hello = "hello";
@@ -40,6 +49,7 @@ public class HelloControllerTest {
                                                         -Controller에서 "hello"를 리턴하기 때문에 이 값이 맞는지 검증합니다.*/
     }
 
+    @WithMockUser(roles = "USER")
     @Test
     public void helloDto가_리턴된다() throws Exception {
         String name = "hello";
